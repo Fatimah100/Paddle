950b42c4459e389032e8b7646619b360aa167037
Fatimah100 <87055913+Fatimah100@users.noreply.github.com>
2021-12-14 12:04:59 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,96 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+const Constraint = Matter.Constraint;
+var bob1,bob2,bob3, bob4,bob5, roofObject
+var rope1,rope2,rope3, rope4,rope5;
+var world;
+
+
+function setup() {
+	createCanvas(800, 600);
+	rectMode(CENTER);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+  var ground_options ={
+    isStatic: true
+  };
+ 
+  var bob_options = {
+    restitution: 0.95,
+    frictionAir:0.01
+
+  }
+
+  var rock_options = {
+    restitution: 0.75,
+    frictionAir:0.01
+  }
+
+	World.add(world,con)
+
+	roofObject=new roof(400,250,230,20);
+	bob1 = new bob(320,575,40)
+	bob2 = new bob(360,575,40)
+	bob3 = new bob(400,575,40)
+	bob4 = new bob(440,575,40)
+	bob5 = new bob(480,575,40)
+	
+	rope1=new rope(bob1.body,roofObject.body,-80)
+	rope2=new rope(bob2.body,roofObject.body,-40)
+	rope3=new rope(bob3.body,roofObject.body,0)
+	rope4=new rope(bob4.body,roofObject.body,40)
+	rope5=new rope(bob5.body,roofObject.body,80)
+	
+	Engine.run(engine);
+	
+  
+}
+
+function draw() {
+  rectMode(CENTER);
+  background(230);
+  roofObject.display();
+
+  rope1.display();
+  rope2.display();
+  rope3.display();
+  rope4.display();
+  rope5.display();
+
+  bob1.display();
+  bob2.display();
+  
+  bob3.display();
+  bob4.display();
+  bob5.display();
+
+  push();
+  strokeWeight(2)
+  stroke(225)
+  line(con.roofOject,con.roofObject.y,bob1.position.x,bob1.position.y)
+  pop();
+  line(con.roofOject,con.roofObject.y,bob2.position.x,bob2.position.y)
+  pop();
+  line(con.roofOject,con.roofObject.y,bob3.position.x,bob3.position.y)
+  pop();
+  line(con.roofOject,con.roofObject.y,bob4.position.x,bob4.position.y)
+  pop();
+  line(con.roofOject,con.roofObject.y,bob5.position.x,bob5.position.y)
+  pop();
+  Engine.update(engine);
+}
+
+function vForce(){
+  Matter.Body.applyForce(ball,{x:0,y:0},{x:0.05,y:0});
+}
+
+if(keyDown "UP ARROW"){
+ bob.positionX=0.5; 
+}

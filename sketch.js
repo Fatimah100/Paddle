18a3a3c515e60dd96d6cb4dac76616cf700654ee
Fatimah100 <87055913+Fatimah100@users.noreply.github.com>
2022-03-03 11:39:09 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -57,7 +57,7 @@ function draw() {
 
 function keyPressed(){
 if(keyCode===UP_ARROW){
-  Matter.Body.applyForce(ball,{x:0,y:0},{x:0.05,y:0});
+  Matter.Body.applyForce(bob1.body,bob1.body.position,{x:-50,y:-45});
 }
 }
 
